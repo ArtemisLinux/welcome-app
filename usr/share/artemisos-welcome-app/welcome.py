997fac17663d20184a7dc75c5340ac4dfffd5f23
frazer <frazer_grant@outlook.com>
2023-08-23 11:42:46 +0100

    UPDATE

---
+++ usr/share/artemisos-welcome-app/welcome.py
@@ -0,0 +1,89 @@
+## Artemis OS Welcome
+## Maintained by: Frazer Grant
+## Version: 0.1
+
+
+
+## Modules to import ##
+import sys
+import subprocess
+from PyQt5 import QtCore
+from PyQt5.QtCore import QCoreApplication, QProcess
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSizePolicy, QPlainTextEdit, QCheckBox
+from PyQt5.uic import loadUi
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
+
+
+
+
+
+## Main Window ##
+class MainUI(QMainWindow):
+    def __init__(self):
+        super(MainUI, self).__init__()
+        loadUi("/usr/share/artemisos-welcome-app/welcome.ui", self)
+        width = 640
+        height = 480
+        self.setFixedSize(width, height)
+        self.p = QProcess() 
+
+## Buttons ##
+        self.output = QPlainTextEdit(self.output1)
+        self.output.setReadOnly(True)
+        self.close.clicked.connect(QCoreApplication.instance().quit)
+        self.website.clicked.connect(self.openwebsite)
+        self.github.clicked.connect(self.githubopen)
+        self.update.clicked.connect(self.updatesys)
+        self.pacfix.clicked.connect(self.gpgfix)
+        self.clearbox.clicked.connect(self.clearmsgbox)
+        self.autostart.stateChanged.connect(self.clickBox)
+
+
+
+
+
+## Definitions ## 
+    def openwebsite(self):
+        subprocess.run(["xdg-open", "https://artemislinux.github.io/"])
+    
+    def githubopen(self):
+        subprocess.run(["xdg-open", "https://github.com/ArtemisLinux"])
+
+    def message(self, s):
+        self.output.appendPlainText(s)
+
+    def updatesys(self):
+        self.message("Updating system, Please wait...")
+        self.p.finished.connect(self.process_finished)  # Clean up once complete.
+        self.p.start("kitty", ["update"])
+
+    def gpgfix(self):
+        self.message("Fixing GPG key.")
+        self.p.finished.connect(self.process_finished)  # Clean up once complete.
+        self.p.start("kitty", ["gpgfix"])
+
+    def process_finished(self):
+        self.message("Process finished.")
+        #self.p = None
+
+    def clearmsgbox(self):
+        self.output.clear() 
+
+
+    def clickBox(self, state):
+
+        if state == QtCore.Qt.Checked:
+         self.p.start("kitty", ['./autostart_enable'])
+        else:
+         self.p.start("kitty", ['./autostart_disable'])
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MainUI()
+    ui.show()
+    app.exec_()
+
+
+
+
